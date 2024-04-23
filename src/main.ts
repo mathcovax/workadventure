f191d8276e60a42e424cbdb690a397cec09117a9
mathcovax <campani.mathieu@gmail.com>
2024-04-23 12:51:24 +0000

    add rage setup function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-export interface test {}
+import type {WorkAdventureApi} from "@workadventure/iframe-api-typings"
 
-export default function rageMode(){
-    
+export default async function rageMode(
+    wa: WorkAdventureApi,
+){
+    wa.room.setTiles([{x: 10, y: 10, tile: 1, layer: "start1"}])
 }
