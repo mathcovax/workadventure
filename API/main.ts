6f90c2351a161a5061d00058e3af7abe33dd6776
mathcovax <campani.mathieu@gmail.com>
2024-04-23 22:30:17 +0000

    add proxy

---
+++ API/main.ts
@@ -0,0 +1,108 @@
+import {createReadStream} from "fs"
+import Fastify from 'fastify';
+import cors from '@fastify/cors'
+
+const fastify = Fastify({})
+
+fastify.register(cors, { 
+    origin: /[^]*/
+})
+
+fastify.get("/*", (request, reply) => {
+    const url = new URL(`https://${request.url}`)
+    if(url.pathname.endsWith(".tmj")){
+        fetch(
+            url.href,
+            {
+                method: "GET",
+                headers: {
+                    ...request.headers as Record<string, string>,
+                    host: url.host
+                },
+                redirect: "follow",
+            }
+        )
+        .then(response => response.json())
+        .then((map) => {
+            const baseTileLayer = map.layers.find(v => v.type === "tilelayer");
+            if(!baseTileLayer) {
+                throw new Error("No base found");
+            }
+    
+            const cloneTileLayer = {
+                ...baseTileLayer,
+                data: baseTileLayer.data.map(() => 0),
+                visible: true,
+                name: "rage",
+                id: 9999999
+            }
+    
+            map.layers.push(cloneTileLayer);
+    
+            const lastTileset = map.tilesets[map.tilesets.length - 1]
+    
+            const cloneTileset = {
+                ...lastTileset,
+                firstgid: lastTileset.firstgid + lastTileset.tilecount,
+                tilecount: 8,
+                image: "",
+            }
+
+            const scriptProp = map.properties.find(v => v.name === "script")
+            scriptProp.value
+    
+            reply.send(map)
+        })
+        .catch((error) => {
+            console.log(error);
+            reply.code(500).send(error)
+        })
+    }
+    else if(url.pathname.endsWith(".png") || true){
+        fetch(
+            url.href,
+            {
+                method: "GET",
+                headers: {
+                    ...request.headers as Record<string, string>,
+                    host: url.host
+                },
+                redirect: "follow",
+            }
+        )
+        .then(response => {
+            const headers = {};
+            response.headers.forEach((value, key) => {
+                headers[key] = value;
+            });
+    
+            reply.raw.writeHead(response.status, headers)
+    
+            const reader = response.body?.getReader()
+            if(reader){
+                reader.read().then(function pump({done, value}){
+                    if(value){
+                        reply.raw.write(value);
+                    }
+        
+                    if(!done){
+                        reader.read().then(pump);
+                    }
+                    else {
+                        reply.raw.end();
+                    }
+                })
+            }
+            else {
+                reply.raw.end();
+            }
+    
+        })
+        .catch((error) => {
+            console.log(error);
+            reply.code(500).send(error)
+        })
+    }
+})
+
+fastify.listen({ port: 1506, host: "0.0.0.0" })
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import type {WorkAdventureApi} from "@workadventure/iframe-api-typings"
 export default async function rageMode(
     wa: WorkAdventureApi,
 ){
-    wa.room.setTiles([{x: 10, y: 10, tile: 1, layer: "start1"}])
+    wa.room.setTiles([{x: 10, y: 10, tile: 2956, layer: "start1"}])
 }
