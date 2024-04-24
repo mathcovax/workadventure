4f5ae06d3d31d29c49ed496db7a79c76e49b9eea
Houmame LAZAR <houms937@gmail.com>
2024-04-24 13:54:47 +0200

    animated tile piano

--- API/utils.ts
+++ API/utils.ts
@@ -1,28 +1,11 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
+import pianoTile from "./tiles/piano.tile";
 
-export function animatedTileset(initialTileset) {
-    let nextFirstGid: number = initialTileset.firstgid + initialTileset.tilecount;
-    let tilesets = [];
-let tiles = fs.readdirSync(path.join(__dirname, "tiles"));
-    tiles.forEach(file => {
-        const filePath = path.join(__dirname, "tiles", file);
-        const stats = fs.statSync(filePath);
-
-        if (stats.isDirectory()) {
-            tilesets = tilesets.concat(animatedTileset(initialTileset));
-        } else if (stats.isFile() && path.extname(file) === '.json') {
-            try {
-                let fileContents = fs.readFileSync(filePath, { encoding: 'utf-8' });
-                let tileset = JSON.parse(fileContents);
-                tileset.firstgid = nextFirstGid;
-                tilesets.push(tileset);
-                nextFirstGid += tileset.tilecount;
-            } catch (error) {
-                console.error(`Error processing file ${file}:`, error);
-            }
+export function animated_tileset(tileset){
+    let nextfirstgid = tileset.firstgid + tileset.tilecount;
+    return [
+        {
+            ...pianoTile,
+            firstgid: nextfirstgid
         }
-    });
-
-    return tilesets;
+    ]
 }
